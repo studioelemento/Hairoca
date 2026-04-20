8609eec3acffaa9f02382dcbf1baf0271b7c7719
Muhammed Niyas <niyashussain10@gmail.com>
2026-04-20 11:16:53 +0530

    Reduce header height: lowered padding and logo size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header>
       <div className="container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <a href="#" className="logo" style={{ display: 'flex', alignItems: 'center' }}>
-          <img src="/logo.avif" alt="Hairoca Logo" style={{ maxHeight: '60px', width: 'auto', display: 'block' }} />
+          <img src="/logo.avif" alt="Hairoca Logo" style={{ maxHeight: '45px', width: 'auto', display: 'block' }} />
         </a>
       </div>
     </header>
